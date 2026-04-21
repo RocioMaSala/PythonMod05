658350cd40cf399fbe335f437f970ee5cde7433b
RocioMaSala <romarti2@student.42madrid.com>
2026-04-21 23:24:12 +0200

    21.4

---
+++ ex0/data_processor.py
@@ -0,0 +1,26 @@
+import abc
+import typing
+
+
+class DataProcessor(abc):
+    def validate():
+        asdf
+    
+    def ingest():
+        asdf
+    
+    def output():
+        asdf
+
+
+class NumericProcessor(DataProcessor):
+    asdfads
+
+
+class TextProcessor(DataProcessor):
+    asdf
+
+
+class LogProcessor(DataProcessor):
+    adfadsf
+
